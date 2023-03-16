7b7df360c3ae208e8c809ef6a4c63627001b5489
Surya Yallamelli <81527032+SuryaYallamelli@users.noreply.github.com>
2023-03-16 22:56:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const addButton = document.getElementById("add");
+const editButton = document.getElementById("edit");
+const moveButton = document.getElementById("move-up");
+const moveDownButton = document.getElementById("move-down");
+const deleteButton = document.getElementById("delete");
+const refreshButton = document.getElementById("refresh");
+const saveButton = document.getElementById("save");
+
+function render() {}
+function add() {}
+function edit() {}
+function moveUp() {}
+function moveDown() {}
+function deleter() {}
+function refresh() {}
+function save() {}
+function sortAscending() {}
+function sortDescending() {}
+function generateId() {}
