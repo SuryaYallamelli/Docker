57283e5335aceee5bc59efafd023a57f5f5d7947
Surya Yallamelli <81527032+SuryaYallamelli@users.noreply.github.com>
2023-03-17 14:51:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,15 +5,36 @@ const moveDownButton = document.getElementById("move-down");
 const deleteButton = document.getElementById("delete");
 const refreshButton = document.getElementById("refresh");
 const saveButton = document.getElementById("save");
+const selectall = document.getElementById("chkbox");
 
 function render() {}
+
 function add() {}
+
 function edit() {}
+
 function moveUp() {}
+
 function moveDown() {}
+
 function deleter() {}
+
 function refresh() {}
+
 function save() {}
+
 function sortAscending() {}
+
 function sortDescending() {}
+
 function generateId() {}
+
+
+
+/*-----------------------------
+code for select all button
+------------------------------*/
+
+selectall.addEventListener('click', function() {
+    console.log("Clicked!");
+});
